aa00eed13492d407886556b2bd55bfcd60f6c16f
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-08-13 01:22:03 +0900

    feat get users

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostService } from './post/post.service';
+import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PrismaService, PostService],
 })
 export class AppModule {}
