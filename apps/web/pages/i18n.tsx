b33cf670101e74bcea31ba0835a0d8431a75a604
tomoki <sk.mk.ys.mt@gmail.com>
2021-12-06 10:45:54 +0900

    rm: react-intl

--- apps/web/pages/i18n.tsx
+++ apps/web/pages/i18n.tsx
@@ -1,19 +1,14 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { useIntl } from 'react-intl';
 
 const I18n: NextPage = () => {
-  const { formatMessage } = useIntl();
-
   return (
     <div>
       <Head>
         <title>i18n</title>
       </Head>
 
-      <div>{formatMessage({ id: 'm3HSJL', defaultMessage: 'hello' })}</div>
-      <div>{formatMessage({ id: 'EYU99A', defaultMessage: 'world' })}</div>
-      <div>{formatMessage({ defaultMessage: 'hoge' })}</div>
+      <div>hello</div>
     </div>
   );
 };
